d60d2c883c2bb21bab6ca848899ae48a970e741a
MannyAsh <amaan.ashiq.aa@gmail.com>
2023-09-15 15:39:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmup1diff21
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            
+
+            Console.WriteLine(diff21(100));
+            Console.ReadLine();
+
+        }
+
+        static int diff21(int n) {
+
+            int number = 21;
+
+            if (n <= number)
+            {
+
+                int sum = number - n;
+                return sum;
+            }
+
+            else {
+
+                int sum2 = (n - number) * 2;
+                return sum2;
+            
+            } 
+           
+            
+           
+        
+        }
+    }
+}
